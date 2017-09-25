3239c6c6c4a8ed64f496db85c4b5b2a869a5f77e
cullenm <cmarchenko@csumb.edu>
2017-09-25 20:33:43 +0000

    linked hw2, lab2, lab3

---
+++ index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<html>
+    <a href="./Homework/hw2/index.php">Homework 2</a>
+    <a href="./lab/777/index.php">Lab 2</a>
+    <a href="./lab/lab3/silverjack.php">Lab 3</a>
+</html>
